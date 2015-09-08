bf24a34504b86b0afd4df963c98798212d4e66c3
Jodie Burchell <jodie.burchell@gmail.com>
2015-09-08 22:23:50 +1000

    I forgot configuration files

---
+++ fabfile.py
@@ -0,0 +1,58 @@
+from fabric.api import *
+import fabric.contrib.project as project
+import os
+import shutil
+import sys
+import SocketServer
+
+from pelican.server import ComplexHTTPRequestHandler
+
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'output'
+DEPLOY_PATH = env.deploy_path
+
+# Port for `serve`
+PORT = 8000
+
+def clean():
+    """Remove generated files"""
+    if os.path.isdir(DEPLOY_PATH):
+        shutil.rmtree(DEPLOY_PATH)
+        os.makedirs(DEPLOY_PATH)
+
+def build():
+    """Build local version of site"""
+    local('pelican -s pelicanconf.py')
+
+def rebuild():
+    """`clean` then `build`"""
+    clean()
+    build()
+
+def regenerate():
+    """Automatically regenerate site upon file modification"""
+    local('pelican -r -s pelicanconf.py')
+
+def serve():
+    """Serve site at http://localhost:8000/"""
+    os.chdir(env.deploy_path)
+
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), ComplexHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
+
+def reserve():
+    """`build`, then `serve`"""
+    build()
+    serve()
+
+def preview():
+    """Build production version of site"""
+    local('pelican -s publishconf.py')
+
+def publish():
+    """Publish to production via rsync"""
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = u'Jodie Burchell'
+SITENAME = u'Standard error'
+
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Melbourne'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# These are my nice configurations
+THEME = os.path.join(os.path.dirname(os.path.realpath(__file__)), "theme")
+USE_MATHJAX = True
+ARCHIVES_URL = 'archives.html'
+TYPOGRIFY = True
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{slug}.html'
+
+AUTHOR_GITHUB = 't-redactyl'
+AUTHOR_TWITTER = 't_redactyl'
+
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.png', 'figure']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.png': {'path': 'favicon.png'}
+}
+SITESUBTITLE = u'from scientist to data scientist'
+SITE_DESCRIPTION = u"My name is Jodie Burchell and I'm a data scientist living in the beautiful city of Melbourne, Australia. This blog is a collection of my data science projects and things I've learned using R, Python, Stata and other tools."
