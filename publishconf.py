330ead39b43dd3516cc55d9e4b81d15fbb7edd1b
Jodie Burchell <jodie.burchell@gmail.com>
2025-06-21 13:28:12 +0200

    Update site URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://t-redactyl.github.io'
+SITEURL = 'https://t-redactyl.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
